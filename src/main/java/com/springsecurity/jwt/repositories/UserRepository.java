8be429e94a6e61ee02ecc3cde3753eb0950ed00c
HuyHieu <hieunh3010@gmail.com>
2022-06-13 14:34:35 +0700

    jwt the first

---
+++ src/main/java/com/springsecurity/jwt/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.springsecurity.jwt.repositories;
+
+import com.springsecurity.jwt.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUsername(String username);
+}
